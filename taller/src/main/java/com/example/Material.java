de78291bd7a789f7b6b04cac02c8e4251e571924
Cristian1BD <162839460+Cristian1BD@users.noreply.github.com>
2024-10-19 16:07:11 -0500

    a2

--- taller/src/main/java/com/example/Material.java
+++ taller/src/main/java/com/example/Material.java
@@ -1,5 +1,43 @@
 package com.example;
 
-public class Material {
+public abstract class Material {
+    private String titulo;
+    private String autor;
+    private int añoPublicacion;
+    private boolean disponible;
 
+    public Material(String titulo, String autor, int añoPublicacion) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.añoPublicacion = añoPublicacion;
+        this.disponible = true; // Por defecto, el material está disponible
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Año de publicación: " + añoPublicacion);
+        System.out.println("Disponible: " + (disponible ? "Sí" : "No"));
+    }
+
+    public void prestar() {
+        if (disponible) {
+            disponible = false;
+            System.out.println("El material ha sido prestado.");
+        } else {
+            System.out.println("El material no está disponible.");
+        }
+    }
+
+    public void devolver() {
+        if (!disponible) {
+            disponible = true;
+            System.out.println("El material ha sido devuelto.");
+        } else {
+            System.out.println("El material ya está disponible.");
+        }
+    }
+
+    public abstract String getTipoMaterial();
+    public abstract double calcularTarifaPrestamo(int diasPrestamo);
 }
--- taller/src/main/java/com/example/libro.java
+++ taller/src/main/java/com/example/libro.java
@@ -1,5 +1,31 @@
 package com.example;
 
-public class libro {
+public class libro extends Material{
+    private String isbn;
+    private int Numerodepaginas;
 
+    public libro(String titulo, String autor, int añoPublicacion, String isbn, int numerodepaginas) {
+        super(titulo, autor, añoPublicacion);
+        this.isbn = isbn;
+        this.Numerodepaginas = numerodepaginas;
+    }
+    
+    @Override
+    public String getTipoMaterial() {
+        return "Libro";
+    }
+    
+    @Override
+    public double calcularTarifaPrestamo(int diasPrestamo) {
+        double tarifa = 1.5;
+        if (Numerodepaginas > 500) {
+            tarifa += 0.05;
+        }
+        return tarifa * diasPrestamo;
+    }
+    public void mostrarDetallesDelLibro(){
+        System.out.println(" ISBN " + isbn );
+        System.out.println("Número de páginas " + Numerodepaginas );
+    }
+    
 }
